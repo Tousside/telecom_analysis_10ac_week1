9e4fe614a4020ed5a43da032dce91770636e042e
Tousside <toussaint.amoussouvi@yahoo.com>
2024-04-28 11:00:27 +0100

    Update requirements

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="compute_distance",
+    version="0.0.1",
+    package= find_packages(),
+    install_requires=[],
+    author="koomi",
+    description="distance between individual and the centroid of less engaged or experienced group"
+    # long_description=open("README.md").read(),
+    # long_description_content_type="text/markdown",
+)
